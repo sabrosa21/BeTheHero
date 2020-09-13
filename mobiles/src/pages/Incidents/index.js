0f23e6dfc4e02ca2740baadce9e92b511e5b8551
Eurico Correia <sabrosa1990@gmail.com>
2020-09-13 23:26:47 +0100

    style applied to incidents

--- mobiles/src/pages/Incidents/index.js
+++ mobiles/src/pages/Incidents/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { View, Image, Text } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { View, FlatList, Image, Text, TouchableOpacity } from "react-native";
 
 import logoImg from "../../assets/logo.png";
 
@@ -18,6 +19,28 @@ export default function Incidents() {
       <Text style={styles.description}>
         Save the day by choosing one of the following cases:
       </Text>
+
+      <FlatList
+        style={styles.ncidentList}
+        data={[1, 2, 3]}
+        renderItem={() => (
+          <View style={styles.incident}>
+            <Text style={styles.incidentProperty}>NGO:</Text>
+            <Text style={styles.incidentValue}>APAD</Text>
+
+            <Text style={styles.incidentProperty}>CASE:</Text>
+            <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+
+            <Text style={styles.incidentProperty}>VALUE:</Text>
+            <Text style={styles.incidentValue}>120,00 €</Text>
+
+            <TouchableOpacity style={styles.detailsButton} onPress={() => {}}>
+              <Text style={styles.detailsButtonText}>See more details</Text>
+              <Feather name="arrow-right" size={16} color="#E02041" />
+            </TouchableOpacity>
+          </View>
+        )}
+      />
     </View>
   );
 }
